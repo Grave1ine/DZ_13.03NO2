2d903ac40f60b57f6b6c17d5de8de26b77b0c3fc
Grave1ine <vladislav_barinov@bk.ru>
2023-03-15 16:38:17 +0300

    ...

---
+++ src/Summ.java
@@ -0,0 +1,12 @@
+public class Summ {
+    public static void main(String[] args) {
+
+        double A = 42.57;
+        int C = 58;
+
+        System.out.printf("\n%.3f",A/C);`
+        System.out.printf("\n%.3f",A+C);
+        System.out.printf("\n%.3f",A*C);
+        System.out.printf("\n%.3f",A-C);
+    }
+}
